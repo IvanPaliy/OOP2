ef6ad1bcbe18cd0b47e8f28dd68d7f7ff781e4fe
Ivan <ipz202_piv@student.ztu.edu.ua>
2020-12-10 18:58:22 +0200

    OOP2

---
+++ OOP2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Program
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+            Person person = new Person("Ivan", "Ivanov", new Date (12, 2, 19));
+            person.ShowInfo();
+
+
+
+
+
+         }
+    }
+}
